8acd30ff268be170f7df56f554395df24e24c99a
Tony <arinaldi@vt.edu>
2022-01-23 12:25:22 -0600

    Do not wait for data processing for API to respond

--- pages/api/mail.js
+++ pages/api/mail.js
@@ -8,41 +8,39 @@ import {
   sanitizeString,
 } from '../../lib/utils';
 
-function generateMail(releases) {
-  if (releases.length === 0) {
-    const message = 'No results today';
-    const htmlMessage = `<p>${message}</p>`;
+async function generateMail() {
+  try {
+    const data = await getArtists();
+    const artists = new Set(data.map((artist) => sanitizeString(artist)));
+    const releases = await getReleases(artists);
 
-    sendMail(htmlMessage, message);
-    return;
+    if (releases.length === 0) {
+      const message = 'No results today';
+      const htmlMessage = `<p>${message}</p>`;
+
+      sendMail(htmlMessage, message);
+      return;
+    }
+
+    const html = getHtmlResults(releases);
+    const text = getTextResults(releases);
+
+    sendMail(html, text);
+  } catch (error) {
+    console.log('Error: ', error.message);
   }
-
-  const html = getHtmlResults(releases);
-  const text = getTextResults(releases);
-
-  sendMail(html, text);
 }
 
 export default async function handler(req, res) {
   const APP_KEY = process.env.APP_KEY;
   const ACTION_KEY = req.headers.authorization;
-  console.log('keys', APP_KEY, ACTION_KEY);
 
   if (req.method === 'POST') {
-    try {
-      if (ACTION_KEY === APP_KEY) {
-        const data = await getArtists();
-        const artists = new Set(data.map((artist) => sanitizeString(artist)));
-
-        const releases = await getReleases(artists);
-        generateMail(releases);
-
-        res.status(200).json({ success: true });
-      } else {
-        res.status(401).json({ success: false });
-      }
-    } catch (error) {
-      res.status(500).json({ success: false, error: error.message });
+    if (ACTION_KEY === APP_KEY) {
+      generateMail();
+      res.status(200).json({ success: true });
+    } else {
+      res.status(401).json({ success: false });
     }
   } else {
     res.setHeader('Allow', 'POST');
