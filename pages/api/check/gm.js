4ccbcec9b1bff7fe118b5b42eb8b046df61353fb
Tony <arinaldi@vt.edu>
2023-01-15 19:33:23 -0600

    Debug log

--- pages/api/check/gm.js
+++ pages/api/check/gm.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
 
       res.status(200).json({ success: true, releases });
     } catch (error) {
+      console.log('E', JSON.stringify(error));
       res.status(500).json({ success: false, error: error.message });
     }
   } else {
