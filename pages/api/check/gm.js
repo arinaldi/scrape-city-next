08c481146d8f1be2a5cf0377d7b0478fb41ae6e9
Tony <arinaldi@vt.edu>
2023-01-15 19:10:42 -0600

    Scrape second site for releases

--- pages/api/check/gm.js
+++ pages/api/check/gm.js
@@ -1,7 +1,7 @@
-import getArtists from '../../lib/getArtists';
-import getReleases from '../../lib/getReleases';
+import getArtists from '../../../lib/getArtists';
+import getMetalReleases from '../../../lib/getMetalReleases';
 
-import { sanitizeString } from '../../lib/utils';
+import { sanitizeString } from '../../../lib/utils';
 
 export default async function handler(req, res) {
   if (process.env.NEXT_PUBLIC_APP_KEY === req.headers.authorization) {
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
       const data = await getArtists();
       const artists = new Set(data.map((artist) => sanitizeString(artist)));
 
-      const releases = await getReleases(artists);
+      const releases = await getMetalReleases(artists);
 
       res.status(200).json({ success: true, releases });
     } catch (error) {
